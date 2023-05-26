2be2996e0c2b1c9de805425a194ad96e3f628d83
Sneh Aashish Gupta <snehaashishgupta.mnit@gmail.com>
2023-05-26 13:36:33 +0530

    removing comments

--- google_calendar_integration/calendar_integration/views.py
+++ google_calendar_integration/calendar_integration/views.py
@@ -5,10 +5,6 @@ from google.oauth2 import credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 
-
-# Create your views here.
-
-# Constants
 SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
 CLIENT_SECRET_FILE = "C:\\Users\\Lenovo\\Desktop\\Google_Calendar_Django\\google_calendar_integration\\client_secret_82032754664-qgdu8ltsodhm9bt7roa920ve94co16tg.apps.googleusercontent.com.json"
 
@@ -27,15 +23,9 @@ class GoogleCalendarRedirectView(View):
         flow = InstalledAppFlow.from_client_secrets_file(CLIENT_SECRET_FILE, SCOPES, state=state)
         flow.fetch_token(authorization_response=request.build_absolute_uri())
         credentials = flow.credentials
-
-        # Build the Calendar API service
         service = build('calendar', 'v3', credentials=credentials)
-
-        # Get the list of events from the user's calendar
         events_result = service.events().list(calendarId='primary', maxResults=10).execute()
         events = events_result.get('items', [])
-
-        # Process the events (you can customize this part as needed)
         if not events:
             response = "No events found."
         else:
@@ -43,6 +33,5 @@ class GoogleCalendarRedirectView(View):
             for event in events:
                 start = event['start'].get('dateTime', event['start'].get('date'))
                 response += f"- {start} - {event['summary']}\n"
-
         return HttpResponse(response)
 
